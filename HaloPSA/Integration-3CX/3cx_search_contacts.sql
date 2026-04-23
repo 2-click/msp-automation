7bcc4e4e754d80aab83affd48af05b475f3c73b1
Jörn "2_click <16101330+2-click@users.noreply.github.com>
2026-04-23 08:05:56 +0200

    Improved matching
    Refactor SQL query to improve readability and functionality.

--- HaloPSA/Integration-3CX/3cx_search_contacts.sql
+++ HaloPSA/Integration-3CX/3cx_search_contacts.sql
@@ -1,23 +1,33 @@
-SELECT * 
-FROM (
+SELECT * FROM (
     SELECT
-        Uid AS 'contactid',
-        uFirstName AS 'firstname',
-        uLastName AS 'lastname',
-        aareadesc AS 'companyname',
-        uemail AS 'email',
-        CASE
-            WHEN LEFT(REPLACE(REPLACE(REPLACE(uextn, ' ', ''), '-', ''), '/', ''), 1) = '0' 
-            THEN CONCAT('+49', RIGHT(REPLACE(REPLACE(REPLACE(uextn, ' ', ''), '-', ''), '/', ''), LEN(REPLACE(REPLACE(REPLACE(uextn, ' ', ''), '-', ''), '/', '')) - 1))
-            ELSE REPLACE(REPLACE(REPLACE(uextn, ' ', ''), '-', ''), '/', '')
-        END AS 'phonebusiness',
-        CASE
-            WHEN LEFT(REPLACE(REPLACE(REPLACE(umobile2, ' ', ''), '-', ''), '/', ''), 1) = '0' 
-            THEN CONCAT('+49', RIGHT(REPLACE(REPLACE(REPLACE(umobile2, ' ', ''), '-', ''), '/', ''), LEN(REPLACE(REPLACE(REPLACE(umobile2, ' ', ''), '-', ''), '/', '')) - 1))
-            ELSE REPLACE(REPLACE(REPLACE(umobile2, ' ', ''), '-', ''), '/', '')
-        END AS 'phonebusiness2'
-    FROM users
-    JOIN site ON usite = Ssitenum
-    JOIN area ON aarea = sarea
+        u.Uid AS contactid,
+        CASE WHEN n.gen=1 THEN '' ELSE u.uFirstName END AS firstname,
+        CASE WHEN n.gen=1 THEN '' ELSE u.uLastName  END AS lastname,
+        a.aareadesc AS companyname,
+        u.uemail AS email,
+        CASE WHEN LEFT(n.ext,1)='0' THEN '+49'+SUBSTRING(n.ext,2,50) ELSE n.ext END AS phonebusiness,
+        CASE WHEN LEFT(n.m2, 1)='0' THEN '+49'+SUBSTRING(n.m2, 2,50) ELSE n.m2  END AS phonebusiness2,
+        CASE WHEN LEFT(n.m1, 1)='0' THEN '+49'+SUBSTRING(n.m1, 2,50) ELSE n.m1  END AS phonemobile,
+        CASE WHEN LEFT(n.hm, 1)='0' THEN '+49'+SUBSTRING(n.hm, 2,50) ELSE n.hm  END AS phonehome
+    FROM users u
+    JOIN site s ON u.usite = s.Ssitenum
+    JOIN area a ON a.Aarea = s.sarea
+    CROSS APPLY (SELECT
+        REPLACE(REPLACE(REPLACE(ISNULL(u.uextn,   ''),' ',''),'-',''),'/','') AS ext,
+        REPLACE(REPLACE(REPLACE(ISNULL(u.umobile, ''),' ',''),'-',''),'/','') AS m1,
+        REPLACE(REPLACE(REPLACE(ISNULL(u.umobile2,''),' ',''),'-',''),'/','') AS m2,
+        REPLACE(REPLACE(REPLACE(ISNULL(u.utelhome,''),' ',''),'-',''),'/','') AS hm,
+        CASE WHEN u.uFirstName='Allgemeiner' AND u.uLastName='Benutzer' THEN 1 ELSE 0 END AS gen
+    ) n
 ) AS SubQuery
-WHERE (DATALENGTH(phonebusiness) > 0 or DATALENGTH(phonebusiness2) > 0) and (firstname LIKE '%[SearchText]%' or lastname LIKE '%[SearchText]%' or companyname LIKE '%[SearchText]%' or phonebusiness like '%[SearchText]%' or phonebusiness2 LIKE '%[SearchText]%')
+WHERE (DATALENGTH(phonebusiness)  > 0
+    OR DATALENGTH(phonebusiness2) > 0
+    OR DATALENGTH(phonemobile)    > 0
+    OR DATALENGTH(phonehome)      > 0)
+  AND (firstname      LIKE '%[SearchText]%'
+    OR lastname       LIKE '%[SearchText]%'
+    OR companyname    LIKE '%[SearchText]%'
+    OR phonebusiness  LIKE '%[SearchText]%'
+    OR phonebusiness2 LIKE '%[SearchText]%'
+    OR phonemobile    LIKE '%[SearchText]%'
+    OR phonehome      LIKE '%[SearchText]%')
